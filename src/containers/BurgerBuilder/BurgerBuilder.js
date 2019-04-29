d20950a6f6a752f7b54ac8983004d31fd376baa2
andreynho2006 <andreynho2006@gmail.com>
2019-04-29 16:15:49 +0300

    set a state for bulger ingredients, and send ingredient state as props to the Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import HigherOrderComponent from '../../hoc/HigherOrderComponent';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            chesse: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <HigherOrderComponent>
-                <div>
-                    Burger
-                </div>
-                <div>
-                    Buil Controls
-                </div>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Buil Controls</div>
             </HigherOrderComponent>
         );
     }
