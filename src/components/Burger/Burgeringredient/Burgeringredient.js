3afd0dbc5e0d57acf1b3258d511b3ad56404b216
andreynho2006 <andreynho2006@gmail.com>
2019-04-29 16:18:32 +0300

    the the keys of the ingredients with Object.keys, from props.ingredients, map over the keys, for each key return and array with that key, map over it and return a BurgerIngredient component equal to the value of each key

--- src/components/Burger/Burgeringredient/Burgeringredient.js
+++ src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -13,23 +13,23 @@ class BurgerIngredient extends Component {
                 break;
             case ('bread-top'):
                 ingredient = (
-                    <div className={classes.Bread.Top}>
+                    <div className={classes.BreadTop}>
                         <div className={classes.Seeds1}></div>
                         <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
-            case ('meet'):
+            case ('meat'):
                 ingredient = <div className={classes.Meat}></div>;
                 break;
-            case ('cheese'):
+            case ('chesse'):
                 ingredient = <div className={classes.Cheese}></div>;
                 break;
             case ('bacon'):
                 ingredient = <div className={classes.Bacon}></div>;
                 break;
             case ('salad'):
-                ingredient = <div className={classes.Saladt}></div>;
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
