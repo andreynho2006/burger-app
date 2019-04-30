2214cc442219517b90a4cbb1557aa5caed326f67
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 14:27:16 +0300

    make the backdrop to respond onClick and remove sideDrawer id is open

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HigherOrderComponent from '../../hoc/HigherOrderComponent';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <HigherOrderComponent>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </HigherOrderComponent>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer : true
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <HigherOrderComponent>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </HigherOrderComponent>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import HigherOrderComponent from '../../../hoc/HigherOrderComponent';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
     return (
         <HigherOrderComponent>
-            <Backdrop show/>
-            <div className={classes.SideDrawer}>
+            <Backdrop 
+                show={props.open} 
+                clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
