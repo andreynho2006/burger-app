d8923aa3b32b6241d0f002b6c1ab20829d4ab524
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 14:14:51 +0300

    add backdrop to the mobile view

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import HigherOrderComponent from '../../../hoc/HigherOrderComponent';
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <HigherOrderComponent>
+            <Backdrop show/>
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </HigherOrderComponent>
+        
     );
 }
 
