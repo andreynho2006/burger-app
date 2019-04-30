00788ec7c2ff0364347cc2c62a05eb2daed4a8f8
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 12:56:43 +0300

    add a sideDrawer , style it and make it visible only in mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import HigherOrderComponent from '../../hoc/HigherOrderComponent';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <HigherOrderComponent>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
