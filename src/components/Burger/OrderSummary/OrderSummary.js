b5b50d90f46b1a5340cbeeafd7f94913d46e417c
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 12:00:04 +0300

    add btnType and clicked props to Button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HigherOrderComponent from '../../../hoc/HigherOrderComponent';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -19,8 +20,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
         </HigherOrderComponent>
     )
 };
