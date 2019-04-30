29cd81f99665d49f683b434541a4c97c9bcf5920
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 11:59:30 +0300

    add method to continue the purchase and send props cu orderSummary with purchaseCancelled and purchaseContinued methods

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
 
+    purchaseContinueHandler = () => {
+        alert("You continue!!!")
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -86,7 +90,11 @@ class BurgerBuilder extends Component {
         return (
             <HigherOrderComponent>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                         />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
