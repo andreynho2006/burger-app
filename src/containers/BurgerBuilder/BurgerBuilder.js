de408d30326b21ad47cf73770bd48ad040c1c330
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 09:46:32 +0300

    make a variable to hold info about ingredients, check for each ingredient if it's value is less or equal to 0 and set it's value true or false

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,6 +37,9 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0) {
+            return;
+        }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
@@ -49,12 +52,20 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for ( let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+        // { salad: true, meat: true, cheese: false......}
         return (
             <HigherOrderComponent>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     ingredientAdded = {this.addIngredientHandler} 
-                    ingredientRemoved = {this.removeIngredientHandler} />
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </HigherOrderComponent>
         );
     }
