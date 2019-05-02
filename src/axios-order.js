6e087b20bca5706861e37f14fc3c59e5d3488b97
andreynho2006 <andreynho2006@gmail.com>
2019-05-02 20:16:12 +0300

    make a variable to hold an order and post it to firebase

--- src/axios-order.js
+++ src/axios-order.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseIRL : 'https://react-my-burger-af936.firebaseio.com/'
+	baseURL : 'https://react-my-burger-af936.firebaseio.com/'
 });
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order'
 
 
 const INGREDIENT_PRICES = {
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue!!!")
+        // alert("You continue!!!")
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Max Swart",
+                address: {
+                    street: "Test Strre",
+                    zipcode: "3244",
+                    country: "Romania"
+                },
+                email: "test@yahoo.com"
+            },
+            deliveryMethod: "fastest"
+        }
+
+        axios.post('/orders.json', order)
+                .then(response => console.log(response)
+                ).catch(error => console.log(error));
     }
 
     render() {
