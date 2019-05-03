765089ef7b2de6beada5af43595f0cd841450f76
andreynho2006 <andreynho2006@gmail.com>
2019-05-03 16:36:20 +0300

    add a state loadin, set it to false, and when the continue button is press, post a order object to firebase, and set the loading to false to close the spinner and close the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-order'
 
 
@@ -25,7 +26,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice : 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -78,6 +80,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert("You continue!!!")
+        this.setState({ loading: true })
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -94,8 +97,12 @@ class BurgerBuilder extends Component {
         }
 
         axios.post('/orders.json', order)
-                .then(response => console.log(response)
-                ).catch(error => console.log(error));
+                .then(response => {
+                    this.setState({ loading: false, purchasing: false })
+                })
+                .catch(error => {
+                    this.setState({ loading: false, purchasing: false });
+                });
     }
 
     render() {
@@ -106,15 +113,19 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0;
         }
         // { salad: true, meat: true, cheese: false......}
-        return (
-            <HigherOrderComponent>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
+        let orderSummary = <OrderSummary 
                         ingredients={this.state.ingredients}
                         price={this.state.totalPrice}
                         purchaseCancelled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}
                          />
+        if(this.state.loading) {
+            orderSummary = <Spinner />
+        }
+        return (
+            <HigherOrderComponent>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    { orderSummary }
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
